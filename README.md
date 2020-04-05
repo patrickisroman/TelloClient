86a0efeb381907ed2d153bc35cd4a5383470f5ef
Patrick Caruso <patrickec246@gmail.com>
2020-04-04 20:16:12 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Stream tello to cv2
---
+++ stream.py
@@ -0,0 +1,26 @@
+import ffmpeg
+import cv2
+import numpy as np
+
+IMG_W = 960
+IMG_H = 720
+IMG_C = 3
+TELLO_ENDPOINT = 'udp://192.168.10.1:11111'
+
+process1 = (
+    ffmpeg
+    .input(TELLO_ENDPOINT)
+    .output('pipe:', format='rawvideo', pix_fmt='rgb24')
+    .run_async(pipe_stdout=True)
+)
+
+while True:
+	in_bytes = process1.stdout.read(IMG_W * IMG_H * IMG_C)
+	if not in_bytes:
+		break
+	inframe = np.frombuffer(in_bytes, np.uint8)
+	inframe = inframe.reshape((IMG_H, IMG_W, IMG_C))
+	cv2.imshow('outshow', inframe)
+
+process1.wait()
+cv2.destroyAllWindows()
