d31c641ac1dbe139270e13d7a15fb28a59ed2676
Patrick Caruso <patrickec246@gmail.com>
2020-04-22 21:51:49 -0700

    Tello cv2 streaming via ffmpeg

--- tello_stream.py
+++ tello_stream.py
@@ -14,13 +14,21 @@ def start_stream():
 		.output('pipe:', format='rawvideo', pix_fmt='rgb24')
 		.run_async(pipe_stdout=True)
 	)
+
+	cv2.startWindowThread()
+	cv2.namedWindow('tello_stream')
+
 	while True:
 		in_bytes = process1.stdout.read(IMG_W * IMG_H * IMG_C)
 		if not in_bytes:
 			break
+
 		inframe = np.frombuffer(in_bytes, np.uint8)
 		inframe = inframe.reshape((IMG_H, IMG_W, IMG_C))
-		cv2.imshow('outshow', inframe)
 
-	process1.wait()
+		cv2.imshow('tello_stream', inframe)
+
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+
 	cv2.destroyAllWindows()
